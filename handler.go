826c5dd3d2ca823f1df330be04be09b39bf3408b
Paolo Brunasti <paolo@brunasti.it>
2021-02-06 17:03:55 +0100

    Application improvement

--- handler.go
+++ handler.go
@@ -2,16 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func handler(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
+	log.Printf("/ [%v]\n", request.URL.Path[1:])
+	n, err := fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
+	if err != nil {
+		log.Printf("/ [%v] - Error processing [%v][%d]\n", request.URL.Path[1:], err, n)
+	}
+}
+
+func healthcheck(writer http.ResponseWriter, request *http.Request) {
+	log.Printf("/healthcheck [%v]\n", request.URL.Path[1:])
+	n, err := fmt.Fprintf(writer, "ok")
+	if err != nil {
+		log.Printf("/healthcheck - Error processing [%v][%d]\n", err, n)
+	}
 }
 
 func main() {
 	fmt.Println(appName + " ------------")
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/healthcheck", healthcheck)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(appName + " -------- end")
 }
